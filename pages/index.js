f7f2ecd66aa7537187c82a73eb2688bae612b111
Stefanie <stefcodes@gmail.com>
2023-04-09 22:30:26 -0700

    add output

--- pages/index.js
+++ pages/index.js
@@ -61,6 +61,19 @@ const Home = () => {
             </div>
               </a>
             </div>
+              {/* New code I added here */}
+              {apiOutput && (
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Output</h3>
+                  </div>
+                </div>
+                <div className="output-content">
+                  <p>{apiOutput}</p>
+                </div>
+              </div>
+              )}
           </div>
         </div>
       </div>
