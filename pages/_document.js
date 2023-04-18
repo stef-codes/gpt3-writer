cfcec7a032e3e9df9173ee32ff68daa72c045d1b
Stefanie <stefcodes@gmail.com>
2023-04-17 17:19:42 -0700

    update analytics

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,6 @@ export default function Document() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'G-1RM9PF9TSQ');
         </script>
       </Head>
