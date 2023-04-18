38e1ed75f0aa5383a060ea8c9314b6e0fbd4d2d5
Stefanie <stefcodes@gmail.com>
2023-04-17 17:14:48 -0700

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,15 @@ export default function Document() {
           // content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
         />
         {/* <meta name="twitter:card" content="summary_large_image"></meta> */}
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1RM9PF9TSQ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1RM9PF9TSQ');
+        </script>
       </Head>
       <body>
         <Main />
