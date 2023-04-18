92a3b182614aa7e2a472bb92a95110880af92af9
Stefanie <stefcodes@gmail.com>
2023-04-17 17:22:48 -0700

    update analytics

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-1RM9PF9TSQ"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
           gtag('config', 'G-1RM9PF9TSQ');
         </script>
